9c74cde51c0e6df8e5e47a47c8c2342d1f08e56d
jungtaelee0128 <jungtaelee0128@gmail.com>
2021-09-21 14:37:52 -0400

    proj2 pushing

---
+++ Flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flixster
+//
+//  Created by Jung tae Lee on 9/20/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
